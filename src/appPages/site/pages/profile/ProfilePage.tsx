2a4c506668f686a8787c9c64abf774fb6de02c0a
Janybek2007 <islam.janybekov.007@gmail.com>
2025-02-04 23:36:29 +0600

    profile finished

--- src/appPages/site/pages/profile/ProfilePage.tsx
+++ src/appPages/site/pages/profile/ProfilePage.tsx
@@ -1,11 +1,13 @@
 import Breadcrumb from '$/shared/ui/breadcrumb/Breadcrumb';
 import React from 'react';
 import ProfileImages from './sections/profile-images/ProfileImages';
+import ProfileSection from './sections/profile-section/ProfileSection';
+import { profiles } from './constants/profile.constants';
 
 const ProfilePage: React.FC = () => {
 	return (
 		<main>
-			<section className='container'>
+			<section className='container nopadding'>
 				<Breadcrumb
 					items={[
 						{ label: 'Башкы бет', href: '/' },
@@ -14,6 +16,19 @@ const ProfilePage: React.FC = () => {
 				/>
 			</section>
 			<ProfileImages />
+			<ProfileSection
+				first
+				title={profiles['kalyc-zamanbekov'][0].title}
+				content={profiles['kalyc-zamanbekov'][0].content}
+			/>
+			<ProfileSection
+				title={profiles['kalyc-zamanbekov'][1].title}
+				content={profiles['kalyc-zamanbekov'][1].content}
+			/>
+			<ProfileSection
+				title={profiles['kalyc-zamanbekov'][2].title}
+				content={profiles['kalyc-zamanbekov'][2].content}
+			/>
 		</main>
 	);
 };
