fcf664cdea3fe629673ba1e375a76639bcf28d80
rahmanovdev <jumadilrahmonov@gmail.com>
2025-02-05 12:01:40 +0600

    articles

---
+++ src/app/(site)/articles/page.tsx
@@ -0,0 +1,11 @@
+import ArticlesPage from "$/appPages/site/pages/articles/ArticlesPage";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Калыс устаз тарабынан жазылган макалалар  | Макалалар",
+};
+
+const page = () => <ArticlesPage />;
+
+export default page;
--- src/entities/articles/ui/article-card/ArticleCard.tsx
+++ src/entities/articles/ui/article-card/ArticleCard.tsx
@@ -1,39 +1,44 @@
-import React from 'react';
-import { articles } from '../../constants/articles.constants';
-import styles from './ArticleCard.module.scss';
-import Image from 'next/image';
-type ArticleCardProps = (typeof articles)[number];
+import React from "react";
+import { articles } from "../../constants/articles.constants";
+import styles from "./ArticleCard.module.scss";
+import Image from "next/image";
+type ArticleCardProps = (typeof articles)[number] & {
+  type?: "list" | "card";
+  badge?: string;
+};
 
 export const ArticleCard: React.FC<ArticleCardProps> = ({
-	date,
-	description,
-	image_src,
-	title,
-	views
+  date,
+  description,
+  image_src,
+  title,
+  views,
+  type = "card",
+  badge,
 }) => {
-	return (
-		<div className={styles.article_card}>
-			<figure>
-				<Image width={406} height={200} src={image_src} alt='Article Image' />
-			</figure>
-			<div className={styles['card_content']}>
-				<h3 className={styles.title}>{title}</h3>
-				<p className={styles.description}>{description}</p>
-				<div className={styles.row}>
-					<div className={styles.views}>
-						<figure>
-							<Image
-								width={28}
-								height={28}
-								src={'/icon/view.svg'}
-								alt='Article Image'
-							/>
-						</figure>
-						<span>{views}</span>
-					</div>
-					<span className={styles.date}>{date}</span>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.article_card}>
+      <figure>
+        <Image width={406} height={200} src={image_src} alt="Article Image" />
+      </figure>
+      <div className={styles["card_content"]}>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.description}>{description}</p>
+        <div className={styles.row}>
+          <div className={styles.views}>
+            <figure>
+              <Image
+                width={28}
+                height={28}
+                src={"/icon/view.svg"}
+                alt="Article Image"
+              />
+            </figure>
+            <span>{views}</span>
+          </div>
+          <span className={styles.date}>{date}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
