7c14c388ce5861da43afa63a4ad17dd4f4ac1a11
Janybek2007 <islam.janybekov.007@gmail.com>
2025-02-05 16:15:40 +0600

    redesigned article-card

--- src/entities/articles/index.ts
+++ src/entities/articles/index.ts
@@ -1,3 +1,3 @@
-export { ArticleCard } from './ui/article-card/ArticleCard';
+export { ArticleItem } from './ui/article-item/ArticleItem';
 export { ArticlesList } from './ui/articles-list/ArticlesList';
 export { BackButton } from './ui/back-button/BackButton';
